350a48e6d5a8d26732661c357e590f8773664e4c
Akiff Manji <amanji@petridish.dev>
2023-12-05 18:24:58 +0000

    chore: configure required packages
    Signed-off-by: Akiff Manji <amanji@petridish.dev>

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
+    webpack: (config, { }) => {
+        config.resolve.alias['react-native$'] = 'react-native-web'
+        return config
+    },
+}
 
 module.exports = nextConfig
